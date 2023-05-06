5b50b748b8c6b25fdab05dfc4aee8683e01ccd7a
Vishwas472 <sanjaydangar31@gmail.com>
2023-05-06 10:39:38 +0530

    update

---
+++ app/src/main/java/com/example/myfirebase/FacebookLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacebookLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facebook_login)
+    }
+}
