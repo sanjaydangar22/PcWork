3e34ef1b7cbcdf77538d06e4c6a8b9d0b37e8ca5
Vishwas472 <sanjaydangar31@gmail.com>
2023-05-08 10:33:01 +0530

    update

--- app/src/main/java/com/example/myfirebase/FacebookLoginActivity.kt
+++ app/src/main/java/com/example/myfirebase/FacebookLoginActivity.kt
@@ -1,11 +1,83 @@
 package com.example.myfirebase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirebase.databinding.ActivityFacebookLoginBinding
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.GraphRequest
+import com.facebook.GraphResponse
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FacebookAuthProvider
+import org.json.JSONException
+import org.json.JSONObject
+import java.util.Arrays
+
 
 class FacebookLoginActivity : AppCompatActivity() {
+
+    lateinit var facebookBinding: ActivityFacebookLoginBinding
+    lateinit var callbackManager: CallbackManager
+    var Email="email"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_facebook_login)
+        facebookBinding = ActivityFacebookLoginBinding.inflate(layoutInflater)
+        setContentView(facebookBinding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+
+
+        callbackManager = CallbackManager.Factory.create()
+        facebookBinding.btnLogin.setReadPermissions(Arrays.asList(Email))
+
+        // Callback registration
+        facebookBinding.btnLogin.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException) {
+
+                }
+
+                override fun onSuccess(result: LoginResult) {
+
+                    var request = GraphRequest.newMeRequest(result.accessToken,
+                        object : GraphRequest.GraphJSONObjectCallback {
+                            override fun onCompleted(obj: JSONObject?, response: GraphResponse?) {
+
+                                    var email = obj?.getString("email")
+
+                                    Log.e("TAG", "onCompleted:"+email)
+
+                            }
+                        })
+
+                    val parameters=Bundle()
+                    parameters.putString("fields","id,name,email,gender,birthday")
+                    request.parameters=parameters
+                    request.executeAsync()
+
+
+
+                    val credential:AuthCredential=FacebookAuthProvider.getCredential(result.accessToken.token)
+
+                }
+
+            })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/com/example/myfirebase/LoginActivity.kt
+++ app/src/main/java/com/example/myfirebase/LoginActivity.kt
@@ -2,9 +2,17 @@ package com.example.myfirebase
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirebase.databinding.ActivityFacebookLoginBinding
 import com.example.myfirebase.databinding.ActivityLoginBinding
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.GraphRequest
+import com.facebook.GraphResponse
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -12,11 +20,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import org.json.JSONObject
+import java.util.Arrays
 
 
 class LoginActivity : AppCompatActivity() {
@@ -25,6 +34,10 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
 
     lateinit var googleSignInClient: GoogleSignInClient
+
+    lateinit var facebookBinding: ActivityFacebookLoginBinding
+    lateinit var callbackManager: CallbackManager
+    var Email="email"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginBinding=ActivityLoginBinding.inflate(layoutInflater)
@@ -71,6 +84,68 @@ class LoginActivity : AppCompatActivity() {
                 ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             )
         }
+
+
+
+        callbackManager = CallbackManager.Factory.create()
+        loginBinding.btnLoginButton.setReadPermissions(Arrays.asList(Email))
+
+        // Callback registration
+        loginBinding.btnLoginButton.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException) {
+
+                }
+
+                override fun onSuccess(result: LoginResult) {
+
+                    var request = GraphRequest.newMeRequest(result.accessToken,
+                        object : GraphRequest.GraphJSONObjectCallback {
+                            override fun onCompleted(obj: JSONObject?, response: GraphResponse?) {
+
+                                var email = obj?.getString("email")
+
+                                Log.e("TAG", "onCompleted:"+email)
+
+                            }
+                        })
+
+                    val parameters=Bundle()
+                    parameters.putString("fields","id,name,email,gender,birthday")
+                    request.parameters=parameters
+                    request.executeAsync()
+
+
+
+                    val credential:AuthCredential= FacebookAuthProvider.getCredential(result.accessToken.token)
+                    // Check credential
+                    firebaseAuth.signInWithCredential(credential)
+                        .addOnCompleteListener{
+                            // Check condition
+                            if (it.isSuccessful) {
+                                // When task is successful redirect to profile activity
+                                startActivity(
+                                    Intent(
+                                        this@LoginActivity,
+                                        MainActivity::class.java
+                                    ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                )
+                                // Display Toast
+                                Toast.makeText(this@LoginActivity, "Firebase authentication successful", Toast.LENGTH_SHORT).show()
+                            } else {
+                                // When task is unsuccessful display Toast
+                                Toast.makeText(this@LoginActivity, "Authentication Failed :", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                }
+
+            })
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
